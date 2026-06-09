4701e3d1ffac78b58623addb4ff41fc722f7b33c
troutmilt@gmail.com <student@bchd.920b9ac1-cdc4-4f74-a708-4afa1bd9ccf9>
2026-06-09 21:02:12 +0000

    terraform and azure resources

---
+++ azure-tf/main.tf
@@ -0,0 +1,32 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the pre-existing resource group assigned to this student"
+}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-tf-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
